2caceaec658eb2ea6623af7ca0ec794fcced9633
Kaleem <Kaleem.peeroo@city.ac.uk>
2019-12-07 04:09:44 +0000

    Added admin login page

---
+++ AdminLogin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Login</title>
+        <link rel="stylesheet" href="css/LoginStyle.css">
+    </head>
+    <body>
+        <div class="column">
+            <h1>Admin Login</h1> 
+
+            <form action="AdminLoginScript.php" method="post">
+            
+                <p class="back-button">
+                    <a href="index.php"><ion-icon name="arrow-round-back"></ion-icon></a>
+                </p>
+
+                <div class="login-input">
+                    <p>
+                        <span class="input-title">Username:</span> <input type="text" placeholder="Username" name="username" autofocus>
+                    </p>
+
+                    <p>
+                    <span class="input-title">Password:</span> <input type="password" placeholder="Password" name="password">
+                    </p>
+                </div>
+
+                <p>
+                    <input class="login-button" type="submit" value="Login">
+                </p>
+
+            </form>
+        </div>
+
+        <!-- Ionicons Import -->
+        <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    </body>
+</html>
